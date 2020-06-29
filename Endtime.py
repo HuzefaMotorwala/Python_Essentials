34ee3a7962a722a31fc0024895b6d92a1caaa594
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-06-29 18:00:43 +0530

    Create Endtime.py

---
+++ Endtime.py
@@ -0,0 +1,11 @@
+hour = int(input("starting time (hours)" ) )
+mins = int(input("starting time (minutes)" ) )
+dura = int(input("event duration (minutes)" ) )
+
+endhour=hour+(dura//60)
+endminute=mins+(dura%60)
+endhour=endhour+(endminute//60)
+endminute=endminute%60
+endhour=endhour%24
+
+print(endhour,endminute,sep=":")
