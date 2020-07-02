7e1125acbdae213b6dec3eeb4972591ef77259cd
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-07-02 15:51:17 +0530

    Create block_pyramid.py

---
+++ block_pyramid.py
@@ -0,0 +1,10 @@
+blocks=int(input("enter the no of blocks:"))
+layers = 0
+height = 0
+
+while layer < blocks:
+    layers += 1
+    blocks -= layers
+    height += 1
+    
+print("Height of the pyramid is:")    
