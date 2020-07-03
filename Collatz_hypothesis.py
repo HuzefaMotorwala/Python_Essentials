0da89718c65c90f16bb62ac3636f84accd33c1f1
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-07-03 13:48:23 +0530

    Update Collatz_hypothesis.py

--- Collatz_hypothesis.py
+++ Collatz_hypothesis.py
@@ -1,1 +1,13 @@
+c0=int(input(""))
+steps = 0
+while c0!=1:
+  if c0 % 2 == 0:
+    c0 = c0//2
+  elif c0 % 2 == 1:
+    c0 = 3 * c0 + 1
+  steps+=1
+  print(c0)
 
+print("steps=",steps)  
+  
+    
