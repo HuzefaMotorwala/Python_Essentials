f6292d08ee3415633a42ab79247b3a8886a903fb
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-07-04 23:09:00 +0530

    Update Bubblesort.py

--- Bubblesort.py
+++ Bubblesort.py
@@ -1,1 +1,10 @@
-
+myList=[4,5,7,2,3,1,8,6]
+swapped=True
+while swapped:
+  swapped = False  #swapped has not occured
+  for i in range(len(myList)-1):
+    if myList[i]>myList[i+1]:
+      myList[i],myList[i+1]=myList[i+1],myList[i]
+      swapped=True
+    
+print(myList)    
