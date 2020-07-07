229ae5204316f63f62d39c330acbec2b6124dde8
Huzefa Motorwala <65020217+HuzefaMotorwala@users.noreply.github.com>
2020-07-07 15:19:07 +0530

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,1 +1,15 @@
-def
+def isleapyear(year):
+  if year%400==0 and year%100==0 or year%4==0:
+    return True
+  else:
+    return False
+  
+testdata=[1900,2000,2016,1987]
+testresult=[False,True,True,False]
+yr=testdata[i]
+print(yr,"=",end="")
+result=isleapyear(yr)
+if result==testresult[i]:
+  print("ok")
+else:
+  print("Failed")
